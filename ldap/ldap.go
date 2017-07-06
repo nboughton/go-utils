8d0eb81335a0ada92e4d3887a9ad306c129d9eec
Nick Boughton <nb5@sanger.ac.uk>
2017-07-06 14:59:44 +0100

    initial commit

---
+++ ldap/ldap.go
@@ -0,0 +1,87 @@
+package ldap
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	"gopkg.in/ldap.v2"
+)
+
+// Config defines the minimum fields required to connect to and search LDAP
+type Config struct {
+	Host   string `json:"host"`
+	Port   int    `json:"port"`
+	User   string `json:"user"`
+	Pass   string `json:"pass"`
+	BaseDN string `json:"baseDN"`
+}
+
+// SangerLdap wraps ldap.Conn so that methods can be attached to it for convenience
+type SangerLdap struct {
+	*ldap.Conn
+	Conf Config
+}
+
+var (
+	// DefaultAttr contains the most likely LDAP Attributes to search for
+	DefaultAttr = []string{"uid", "mail", "cn"}
+)
+
+// ConnectTLS requests a secure binding and requires a Host, Port, User and Pass
+func ConnectTLS(c Config) (*SangerLdap, error) {
+	l, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port), &tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		return &SangerLdap{}, err
+	}
+
+	if err := l.Bind(fmt.Sprintf("cn=%s,%s", c.User, c.BaseDN), c.Pass); err != nil {
+		return &SangerLdap{}, err
+	}
+
+	return &SangerLdap{l, c}, nil
+}
+
+// Connect requests and anonymous binding that requires only a Host and Port
+func Connect(c Config) (*SangerLdap, error) {
+	// Anonymous LDAP connection
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port))
+	if err != nil {
+		return &SangerLdap{}, err
+	}
+	return &SangerLdap{l, c}, nil
+}
+
+// GetEntry attempts to retrieve a users LDAP record based on their numerical UID or string UID,
+// and returns the desired attributes in the attr arg.
+// For example: entry, _ := l.GetEntry(0, "nb5", []string{"cn", "mail"})
+func (l *SangerLdap) GetEntry(UIDnum uint32, UIDStr string, attr []string) (*ldap.Entry, error) {
+	// Constuct search request
+	searchRequest := ldap.NewSearchRequest(
+		fmt.Sprintf("ou=people,%s", l.Conf.BaseDN), // DN
+		ldap.ScopeWholeSubtree,                     // Scope
+		ldap.NeverDerefAliases,                     // Deref Aliases
+		0,     // Size limit
+		0,     // Time limit
+		false, // Types Only?
+		selectUIDFilter(UIDnum, UIDStr), // Filter
+		attr, // Attributes
+		nil,  // Controls
+	)
+
+	// Run search and process results
+	res, err := l.Search(searchRequest)
+	if err != nil {
+		return &ldap.Entry{}, fmt.Errorf("LDAP search error: %s", err.Error())
+	} else if len(res.Entries) != 1 {
+		return &ldap.Entry{}, fmt.Errorf("Invalid UID (%d/%s), %d results matched", UIDnum, UIDStr, len(res.Entries))
+	}
+
+	return res.Entries[0], nil
+}
+
+func selectUIDFilter(n uint32, s string) string {
+	if n == 0 && s != "" {
+		return fmt.Sprintf("(uid=%s)", s)
+	}
+	return fmt.Sprintf("(uidNumber=%d)", n)
+}
---
+++ mail/mail.go
@@ -0,0 +1,41 @@
+package mail
+
+import (
+	"bytes"
+	"fmt"
+	"net/smtp"
+)
+
+var (
+	mailHost = "mail.sanger.ac.uk"
+)
+
+func sendEmail(from, to, subject, msg string) error {
+	// Format the msg text so we get a subject
+	msgFmt := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n", to, subject, msg)
+
+	// Connect to mail host
+	c, err := smtp.Dial(fmt.Sprintf("%s:25", mailHost))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	// Set sender and recipient
+	c.Mail(from)
+	c.Rcpt(to)
+
+	// Stream the body
+	wc, err := c.Data()
+	if err != nil {
+		return err
+	}
+	defer wc.Close()
+
+	buf := bytes.NewBufferString(msgFmt)
+	if _, err := buf.WriteTo(wc); err != nil {
+		return err
+	}
+
+	return nil
+}
