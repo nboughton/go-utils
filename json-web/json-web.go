ea539068648ea81e9b5810e16ebf2cb120126832
Nick Boughton <nb5@sanger.ac.uk>
2017-07-13 15:31:50 +0100

    added json-web

---
+++ json-web/json-web.go
@@ -0,0 +1,17 @@
+package jsonweb
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// JSON is a simple wrapper for json that is then returned to the user
+type JSON struct {
+	Status int         `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
+func (j JSON) write(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(j)
+}
