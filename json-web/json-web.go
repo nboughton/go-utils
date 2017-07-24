9209ee2782d2ccc5bbac8f19e12e3cee7c32cbd6
Nick Boughton <nb5@sanger.ac.uk>
2017-07-24 12:52:52 +0100

    refined response

--- json-web/json-web.go
+++ json-web/json-web.go
@@ -3,15 +3,23 @@ package jsonweb
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 // JSON is a simple wrapper for json that is then returned to the user
 type JSON struct {
-	Status int         `json:"status"`
+	Status int         `json:"-"` // Do not encode status, this goes in the response header
 	Data   interface{} `json:"data"`
 }
 
-func (j JSON) Write(w http.ResponseWriter) error {
+// New creates a new JSON object which can variably accept data
+func New(status int, data interface{}) *JSON {
+	return &JSON{Status: status, Data: data}
+}
+
+// Write sends the JSON object back to the client via browser via the original ResponseWriter
+func (j *JSON) Write(w http.ResponseWriter) error {
+	w.Header().Set("Status", strconv.Itoa(j.Status))
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(j)
 }
