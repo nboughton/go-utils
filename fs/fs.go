440a71405ebb289dc1339968a3c056d8f23dc013
Nick Boughton <nb5@sanger.ac.uk>
2017-08-01 16:20:20 +0100

    Added age funcs

--- fs/fs.go
+++ fs/fs.go
@@ -4,9 +4,25 @@ package fs
 import (
 	"os"
 	"path/filepath"
+	"time"
 )
 
 // GetBinPath returns the absolute path to the directory of the running binary
 func GetBinPath() (string, error) {
 	return filepath.Abs(filepath.Dir(os.Args[0]))
+}
+
+// YearsOld returns the number of years since f modTime was last changed
+func YearsOld(f os.FileInfo) float64 {
+	return time.Now().Sub(f.ModTime()).Hours() / 24 / 365
+}
+
+// WeeksOld returns the number of weeks f modTime was last changed
+func WeeksOld(f os.FileInfo) float64 {
+	return time.Now().Sub(f.ModTime()).Hours() / 24 / 7
+}
+
+// DaysOld returns the number of days f modTime was last changed
+func DaysOld(f os.FileInfo) float64 {
+	return time.Now().Sub(f.ModTime()).Hours() / 24
 }
